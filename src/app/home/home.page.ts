907eb4fe09ca8eb82a9c6b5f098305bdcea633fd
Taylor Jackson <tjackson8@wisc.edu>
2019-07-03 15:53:17 -0500

    initial commit

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-home',
@@ -6,7 +7,22 @@ import { Component } from '@angular/core';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+  constructor(private router: Router) {
 
-  constructor() {}
+  }
+
+  openAbout() {
+    this.router.navigate(['/about']);
+  }
+  openExperience() {
+      this.router.navigate(['/experience']);  // define your component where you want to go
+  }
+  openHome(){
+    this.router.navigate(['/home']);  // define your component where you want to go
+  }
+  // openBlog(){
+  //   this.router.navigate(['/home']);  // define your component where you want to go
+  // }
+
 
 }
