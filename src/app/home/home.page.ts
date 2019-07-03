0b60c77d10dcdf3ab44dc2cef7c89000317dbab1
Taylor Jackson <tjackson8@wisc.edu>
2019-07-03 16:20:15 -0500

    Revert "initial commit"
    This reverts commit 907eb4fe09ca8eb82a9c6b5f098305bdcea633fd.

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,5 +1,4 @@
 import { Component } from '@angular/core';
-import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-home',
@@ -7,22 +6,7 @@ import { Router } from '@angular/router';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
-  constructor(private router: Router) {
 
-  }
-
-  openAbout() {
-    this.router.navigate(['/about']);
-  }
-  openExperience() {
-      this.router.navigate(['/experience']);  // define your component where you want to go
-  }
-  openHome(){
-    this.router.navigate(['/home']);  // define your component where you want to go
-  }
-  // openBlog(){
-  //   this.router.navigate(['/home']);  // define your component where you want to go
-  // }
-
+  constructor() {}
 
 }
